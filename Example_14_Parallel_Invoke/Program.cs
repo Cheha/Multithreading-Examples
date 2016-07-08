38ad6c643add4a0efc2868cd9db0a3a538cb2f90
Alex <steepsanya@bigmir.net>
2016-07-08 07:16:46 +0300

    Parallel Invoke

---
+++ Example_14_Parallel_Invoke/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_14_Parallel_Invoke
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.Invoke(() =>
+            {
+                Console.WriteLine("Hello, Johny");
+            }, () =>
+            {
+                Console.WriteLine("Hello, Mary");
+            }, () =>
+            {
+                Console.WriteLine("Hello, Bob");
+            });
+            Console.ReadLine();
+        }
+    }
+}
