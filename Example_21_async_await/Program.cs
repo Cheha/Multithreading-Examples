da7827f784314bb4ec4954f4a98c29a3b4ed625d
Alex <steepsanya@bigmir.net>
2016-07-10 23:18:41 +0300

    async await example

---
+++ Example_21_async_await/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Example_21_async_await
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMethodAsyck();
+            Console.ReadLine();
+        }
+
+        public static async Task MyMethodAsyck() {
+            Task<int> longRunningTask = LongRunningOperationAsynk();
+
+            int result = await longRunningTask;
+            Console.WriteLine(result);
+        }
+
+        public static async Task<int> LongRunningOperationAsynk()
+        {
+            Thread.Sleep(2000);
+            return 2;
+        }
+    }
+}
